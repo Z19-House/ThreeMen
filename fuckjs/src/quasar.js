bf884c690d6648eac3fe873433afb524f5a8e8f1
Teachoc <Teachoc@outlook.com>
2020-01-03 16:19:41 +0800

    Enable auto dark mode.

--- fuckjs/src/quasar.js
+++ fuckjs/src/quasar.js
@@ -2,11 +2,12 @@ import Vue from 'vue'
 
 import './styles/quasar.sass'
 import '@quasar/extras/material-icons/material-icons.css'
-import { Quasar,Notify } from 'quasar'
+import { Quasar, Notify } from 'quasar'
 
 Vue.use(Quasar, {
   config: {
-    notify: { /* Notify defaults */ }
+    notify: { /* Notify defaults */ },
+    dark: 'auto'
   },
   components: { /* not needed if importStrategy is not 'manual' */ },
   directives: { /* not needed if importStrategy is not 'manual' */ },
